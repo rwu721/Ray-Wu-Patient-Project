23de5a68bf5705c41765d7f5ebf77adadd2e802d
Ray Wu <rwu721@gmail.com>
2012-05-28 16:52:43 -0700

    first commit, closes #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RayWuPatientProject::Application.config.secret_token = 'c62f5510592aa2cb74feddc7e19304605c1ec970afb0be79259d320ef31a814bebcc2c73f3f5793f6caab0dc351c2d15d9dad7dd164351275c6421feef736c99'
