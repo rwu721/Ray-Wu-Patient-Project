e18677a0deaa997a111d03ab456d4b71f39c04d0
Ray Wu <rwu721@gmail.com>
2012-05-29 01:03:47 -0700

    add a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Patient Strength"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
