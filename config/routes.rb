74333c07b70d7a6f744119753aa22555c8df2891
Ray Wu <rwu721@gmail.com>
2012-05-28 17:45:32 -0700

    add a staticpages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RayWuPatientProject::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
